6654fb9ccae533fea21d254fb2746e5b0edbfe57
Oliver Aladrovic <oliver.aladrovic@gmail.com>
2026-04-28 00:47:38 +0200

    Migrate database

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from travel_agency_backend.config import settings
+from travel_agency_backend.config.settings import settings
 from travel_agency_backend.models import Base
 
 # this is the Alembic Config object, which provides
